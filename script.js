d4708946690486ddeee70752cb0133fd6ce6d09e
studentcafe4jj-collab <studentcafe4jj@gmail.com>
2026-01-01 14:47:26 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function addRow(){
- let t=document.getElementById("family");
- let r=t.insertRow();
- for(let i=0;i<4;i++){
-  r.insertCell().innerHTML="<input>";
- }
- r.insertCell().innerHTML="<button onclick='this.parentNode.parentNode.remove()'>Delete</button>";
-}
