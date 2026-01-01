06929d636b634ab6ed8edc3893eccbc793e32bf7
studentcafe4jj-collab <studentcafe4jj@gmail.com>
2026-01-01 14:35:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,8 @@
-function generate(){
-let members=[];
-for(let i=1;i<=10;i++){
-let n=document.getElementById("name"+i).value;
-if(n!=""){
-members.push({
-n:n,
-g:document.getElementById("gender"+i).value,
-a:document.getElementById("age"+i).value,
-r:document.getElementById("rel"+i).value
-});
-}
-}
-
-let data={
-rationno:document.getElementById("rationno").value,
-holder:document.getElementById("holder").value,
-father:document.getElementById("father").value,
-members:members
-};
-
-localStorage.setItem("ration",JSON.stringify(data));
-window.location="ration-card.html";
+function addRow(){
+ let t=document.getElementById("family");
+ let r=t.insertRow();
+ for(let i=0;i<4;i++){
+  r.insertCell().innerHTML="<input>";
+ }
+ r.insertCell().innerHTML="<button onclick='this.parentNode.parentNode.remove()'>Delete</button>";
 }
