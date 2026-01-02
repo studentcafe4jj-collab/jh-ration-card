bc50f2527f63c36d62ea73d79fde3ceaac16905e
studentcafe4jj-collab <studentcafe4jj@gmail.com>
2026-01-02 18:34:44 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
-function addRow(){
- let t=document.getElementById("family");
- let r=t.insertRow();
- for(let i=0;i<4;i++){
-  r.insertCell().innerHTML="<input>";
- }
- r.insertCell().innerHTML="<button onclick='this.parentNode.parentNode.remove()'>Delete</button>";
+function show(id){
+document.querySelectorAll('.section').forEach(s=>s.classList.remove('active'));
+document.getElementById(id).classList.add('active');
 }
