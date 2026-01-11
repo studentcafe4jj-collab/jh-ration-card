cd29b79f554aba5d69645d38692a3a9bc6f75faa
studentcafe4jj-collab <studentcafe4jj@gmail.com>
2026-01-11 09:01:27 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function showSection(id){
-  document.querySelectorAll('.section')
-    .forEach(function(sec){
-      sec.classList.remove('active');
-    });
-  document.getElementById(id).classList.add('active');
-}
-
